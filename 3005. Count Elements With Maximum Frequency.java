7001c0ebe533b5d50119ffa805c7b37683de02a0
workhardR <ronitb739@gmail.com>
2025-12-03 22:27:55 +0530

    leetcode 3005. Count Elements With Maximum Frequency.java

---
+++ 3005. Count Elements With Maximum Frequency.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        int maxFreq = 0;
+        for (int freq : freqMap.values()) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
+
+        int total = 0;
+        for (int freq : freqMap.values()) {
+            if (freq == maxFreq) {
+                total += freq; 
+            }
+        }
+
+        return total;
+    }
+}
