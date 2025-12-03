c12ddafd1dcda78fb9b2021e5ff9dc795c3934dd
workhardR <ronitb739@gmail.com>
2025-12-03 11:42:52 +0530

    leetcode 1590-sum divisible by p

---
+++ 1590. Make Sum Divisible by P.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for (int x : nums) total += x;
+        int need = (int)(total % p);
+        if (need == 0) return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); 
+        long prefix = 0;
+        int ans = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int target = (int)((prefix - need + p) % p);
+            if (map.containsKey(target)) {
+                ans = Math.min(ans, i - map.get(target));
+            }
+            map.put((int)prefix, i);
+        }
+        return ans == nums.length ? -1 : ans;
+    }
+}
