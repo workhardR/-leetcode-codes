b8187a6fbd4bdaf536ac2d5a6cfa41168a2c5b83
workhardR <ronitb739@gmail.com>
2025-12-03 22:14:28 +0530

    leetcode- 11. Container With Most Water.java

---
+++ 11. Container With Most Water.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max = Math.max(max, area);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return max;
+    }
+}
