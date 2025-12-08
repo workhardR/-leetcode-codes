9f1d483aded918c1f012c8bd833517d4eef1efbc
workhardR <ronitb739@gmail.com>
2025-12-08 16:45:49 +0530

    leetcode 1925. Count Square Sum Triples.java

---
+++ 1925. Count Square Sum Triples.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                int c2 = a * a + b * b;
+                int c = (int)Math.sqrt(c2);
+                
+                if (c <= n && c * c == c2) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
